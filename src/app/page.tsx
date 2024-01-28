fa789371d8129f34872f6ead8041dfa8d4f9d831
Jake Lee <github@jakelee.co.uk>
2024-01-28 16:22:13 +0000

    Apply custom style, replace old homepage, etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
 
 import { CreatePost } from "@/app/_components/create-post";
+import { DeletePost } from "@/app/_components/delete-post";
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
+import { late } from "zod";
 
 export default async function Home() {
   noStore();
@@ -11,42 +13,22 @@ export default async function Home() {
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#4e3115] to-[#0f0b07] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Bounty <span className="text-[hsl(27,62%,54%)]">Hour</span>
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-4">
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
+            <h3 className="text-2xl font-bold">Pinging</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+              <b>Ping tRPC: </b>
+              {hello ? hello.greeting : "Loading tRPC query..."}
             </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
+          </div>
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
+            <h3 className="text-2xl font-bold">Authing</h3>
+            <p className="text-lg">
               {session && <span>Logged in as {session.user?.name}</span>}
             </p>
             <Link
@@ -56,29 +38,50 @@ export default async function Home() {
               {session ? "Sign out" : "Sign in"}
             </Link>
           </div>
+          <div className="flex w-full flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
+            <SubmitPost />
+          </div>
+          <div className="flex w-full flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20">
+            <ReadPost />
+          </div>
         </div>
-
-        <CrudShowcase />
       </div>
     </main>
   );
 }
 
-async function CrudShowcase() {
+async function SubmitPost() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest.query();
+  return (
+    <>
+      <h3 className="text-2xl font-bold">Submitting Post</h3>
+      <CreatePost />
+    </>
+  );
+}
+
+async function ReadPost() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const last5Posts = await api.post.getLast5.query();
 
   return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}<br />{latestPost.createdAt.toLocaleString()}</p>
+    <>
+      <h3 className="text-2xl font-bold">Reading Post</h3>
+      {last5Posts.length > 0 ? (
+        last5Posts.map((item) => (
+          <p key={item.id} className="truncate">
+            Post: <i>{item.name}</i> <DeletePost id={item.id} />
+            <br />
+            {item.createdAt.toLocaleString()}
+          </p>
+        ))
       ) : (
         <p>You have no posts yet.</p>
       )}
-
-      <CreatePost />
-    </div>
+    </>
   );
 }
