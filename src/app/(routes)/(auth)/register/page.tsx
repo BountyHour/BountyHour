90487a4ca2562092fe52b344a8326602cda7b4fe
Jake Lee <github@jakelee.co.uk>
2024-01-29 22:23:45 +0000

    Pretty create account screen

--- src/app/(routes)/(auth)/register/page.tsx
+++ src/app/(routes)/(auth)/register/page.tsx
@@ -2,7 +2,6 @@ import { Metadata } from "next";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
-import { UserAuthForm } from "@/components/example/user-auth-form";
 import { ModeToggle } from "@/components/mode-toggle";
 
 import Link from "next/link";
@@ -16,8 +15,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <ModeToggle />
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative grid min-h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/examples/authentication"
           className={cn(
@@ -27,7 +25,7 @@ export default function AuthenticationPage() {
         >
           Login
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="relative hidden min-h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
@@ -43,6 +41,7 @@ export default function AuthenticationPage() {
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
             Acme Inc
+            <ModeToggle />
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
@@ -55,8 +54,8 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="p-8">
+          <div className="mx-auto flex w-[350px] flex-col justify-center space-y-6">
             <DemoCreateAccount />
           </div>
         </div>
