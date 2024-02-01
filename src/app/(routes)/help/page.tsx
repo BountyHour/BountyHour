9f40997826f0672b385c1cc313aebb433f87b13b
Jake Lee <github@jakelee.co.uk>
2024-02-01 19:32:05 +0000

    Proof of concept protecting pages without middleware

--- src/app/(routes)/help/page.tsx
+++ src/app/(routes)/help/page.tsx
@@ -1,12 +1,20 @@
 import { Metadata } from "next";
-import { FormCreateAccount } from "@/app/(routes)/(auth)/register/form-register";
+
+import { redirectIfUnauthed } from "@/lib/auths";
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Help",
   description: "Frequently asked questions",
 };
 
-export default function HelpPage() {
+export default async function HelpPage() {
+  const session = await getServerAuthSession();
+
+  if (!session?.user) {
+    return redirect("/login");
+  }
   return (
     <>
       <div className="container relative grid items-center justify-center lg:max-w-none lg:px-0">
--- src/middleware.ts
+++
@@ -1,5 +0,0 @@
-export { default } from "next-auth/middleware";
-
-// These exact paths require login, any child pages aren't protected
-// Note: Inverse logic is handled in `@/(routes)/(auth)/layout.tsx`
-export const config = { matcher: ["/help", "/account"] };
