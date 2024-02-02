a09ff90adb4d561467ef41d7002b2a40fdac28fa
Jake Lee <github@jakelee.co.uk>
2024-02-02 19:30:53 +0000

    Further improving appearance form

--- src/app/(routes)/profile/appearance/appearance-form.tsx
+++ src/app/(routes)/profile/appearance/appearance-form.tsx
@@ -1,36 +1,10 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ArrowDownAZ } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { useEffect, useState } from "react";
 
 import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 import { useTheme } from "next-themes";
-
-const appearanceFormSchema = z.object({
-  theme: z.enum(["system", "light", "dark"], {
-    required_error: "Please select a theme.",
-  }),
-});
-
-type AppearanceFormValues = z.infer<typeof appearanceFormSchema>;
-
-// This can come from your database or API.
-const defaultValues: Partial<AppearanceFormValues> = {};
 
 function ThemeOption({
   theme,
@@ -45,57 +19,40 @@ function ThemeOption({
   text: string;
   onClick: (theme: string) => void;
 }) {
-  if (previewTheme === "dark") {
-    return (
-      <div
-        onClick={() => onClick(theme)}
-        className={cn(
-          "items-center rounded-md border-2 bg-popover p-1 hover:bg-accent hover:text-accent-foreground",
-          currentTheme === theme ? "border-primary" : "border-muted",
-        )}
-      >
-        <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-          <div className="flex items-center space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-            <div className="h-2 w-[200px] rounded-lg bg-slate-400" />
-          </div>
-          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-            <div className="h-4 w-4 rounded-full bg-slate-400" />
-            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-          </div>
-          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-            <div className="h-4 w-4 rounded-full bg-slate-400" />
-            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-          </div>
+  const background = previewTheme === "dark" ? "bg-slate-950" : "bg-[#ecedef]";
+  const area = previewTheme === "dark" ? "bg-slate-800" : "bg-white";
+  const foreground = previewTheme === "dark" ? "bg-slate-400" : "bg-[#ecedef]";
+
+  return (
+    <div
+      onClick={() => onClick(theme)}
+      className={cn(
+        "items-center rounded-md border-2 bg-popover p-1 hover:bg-accent hover:text-accent-foreground",
+        currentTheme === theme ? "border-primary" : "border-muted",
+      )}
+    >
+      <div className={`space-y-2 rounded-sm ${background} p-2`}>
+        <div
+          className={`flex items-center space-y-2 rounded-md ${area} p-2 shadow-sm`}
+        >
+          <div className={`h-2 w-[200px] rounded-lg ${foreground}`} />
         </div>
-        <span className="block w-full p-2 text-center font-normal">{text}</span>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        onClick={() => onClick(theme)}
-        className={cn(
-          "items-center rounded-md border-2 bg-popover p-1 hover:bg-accent hover:text-accent-foreground",
-          currentTheme === theme ? "border-primary" : "border-muted",
-        )}
-      >
-        <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-          <div className="flex items-center space-y-2 rounded-md bg-white p-2 shadow-sm">
-            <div className="h-2 w-[200px] rounded-lg bg-[#ecedef]" />
-          </div>
-          <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-            <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-          </div>
-          <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-            <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-          </div>
+        <div
+          className={`flex items-center space-x-2 rounded-md ${area} p-2 shadow-sm`}
+        >
+          <div className={`h-4 w-4 rounded-full ${foreground}`} />
+          <div className={`h-2 w-[100px] rounded-lg ${foreground}`} />
         </div>
-        <span className="block w-full p-2 text-center font-normal">{text}</span>
+        <div
+          className={`flex items-center space-x-2 rounded-md ${area} p-2 shadow-sm`}
+        >
+          <div className={`h-4 w-4 rounded-full ${foreground}`} />
+          <div className={`h-2 w-[100px] rounded-lg ${foreground}`} />
+        </div>
       </div>
-    );
-  }
+      <span className="block w-full p-2 text-center font-normal">{text}</span>
+    </div>
+  );
 }
 
 export function AppearanceForm() {
@@ -110,12 +67,12 @@ export function AppearanceForm() {
 
   return (
     <>
-      <form className="grid grid-cols-1 gap-8 pt-2 sm:grid-cols-3 sm:space-y-8">
+      <form className="grid grid-cols-1 gap-8 pt-2 sm:grid-cols-3 ">
         <ThemeOption
           theme="system"
           currentTheme={theme}
           previewTheme={systemTheme}
-          text="System"
+          text="System (automatic)"
           onClick={setTheme}
         />
         <ThemeOption
--- src/app/(routes)/profile/layout.tsx
+++ src/app/(routes)/profile/layout.tsx
@@ -39,7 +39,7 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="m-10 block space-y-6 p-10 pb-16">
+      <div className="m-10 my-16 block space-y-6 md:px-10">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
