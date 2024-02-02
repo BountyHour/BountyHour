af3f2035be11e2e88e6063c26e17021c5987e357
Jake Lee <github@jakelee.co.uk>
2024-02-02 19:16:57 +0000

    Massively simplifies (and fixes) theme picker

--- src/app/(routes)/profile/appearance/appearance-form.tsx
+++ src/app/(routes)/profile/appearance/appearance-form.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowDownAZ } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useEffect, useState } from "react";
 
 import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "@/components/ui/button";
@@ -31,111 +32,107 @@ type AppearanceFormValues = z.infer<typeof appearanceFormSchema>;
 // This can come from your database or API.
 const defaultValues: Partial<AppearanceFormValues> = {};
 
-export function AppearanceForm() {
-  const { setTheme } = useTheme();
+function ThemeOption({
+  theme,
+  currentTheme,
+  previewTheme,
+  text,
+  onClick,
+}: {
+  theme: string;
+  currentTheme: string | undefined;
+  previewTheme: string | undefined;
+  text: string;
+  onClick: (theme: string) => void;
+}) {
+  if (previewTheme === "dark") {
+    return (
+      <div
+        onClick={() => onClick(theme)}
+        className={cn(
+          "items-center rounded-md border-2 bg-popover p-1 hover:bg-accent hover:text-accent-foreground",
+          currentTheme === theme ? "border-primary" : "border-muted",
+        )}
+      >
+        <div className="space-y-2 rounded-sm bg-slate-950 p-2">
+          <div className="flex items-center space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
+            <div className="h-2 w-[200px] rounded-lg bg-slate-400" />
+          </div>
+          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+            <div className="h-4 w-4 rounded-full bg-slate-400" />
+            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+          </div>
+          <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+            <div className="h-4 w-4 rounded-full bg-slate-400" />
+            <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+          </div>
+        </div>
+        <span className="block w-full p-2 text-center font-normal">{text}</span>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        onClick={() => onClick(theme)}
+        className={cn(
+          "items-center rounded-md border-2 bg-popover p-1 hover:bg-accent hover:text-accent-foreground",
+          currentTheme === theme ? "border-primary" : "border-muted",
+        )}
+      >
+        <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
+          <div className="flex items-center space-y-2 rounded-md bg-white p-2 shadow-sm">
+            <div className="h-2 w-[200px] rounded-lg bg-[#ecedef]" />
+          </div>
+          <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+            <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+          </div>
+          <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+            <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+            <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+          </div>
+        </div>
+        <span className="block w-full p-2 text-center font-normal">{text}</span>
+      </div>
+    );
+  }
+}
 
-  const form = useForm<AppearanceFormValues>({
-    defaultValues,
-    resolver: zodResolver(appearanceFormSchema),
-  });
+export function AppearanceForm() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, systemTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
-    <Form {...form}>
-      <form className="space-y-8">
-        <FormField
-          name="theme"
-          render={({ field }) => (
-            <FormItem className="space-y-1">
-              <FormLabel>Theme</FormLabel>
-              <FormDescription>
-                Select the theme for the dashboard.
-              </FormDescription>
-              <FormMessage />
-              <RadioGroup
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                className="grid grid-cols-3 gap-8 pt-2"
-              >
-                <FormItem onClick={() => setTheme("system")}>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="system" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                      <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                        <div className="flex items-center space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-2 w-[200px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                      </div>
-                    </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      System (auto)
-                    </span>
-                  </FormLabel>
-                </FormItem>
-                <FormItem onClick={() => setTheme("light")}>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="light" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-                      <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                        <div className="flex items-center space-y-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-2 w-[200px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                      </div>
-                    </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      Light
-                    </span>
-                  </FormLabel>
-                </FormItem>
-                <FormItem onClick={() => setTheme("dark")}>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="dark" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                      <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                        <div className="flex items-center space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-2 w-[200px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                      </div>
-                    </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      Dark
-                    </span>
-                  </FormLabel>
-                </FormItem>
-              </RadioGroup>
-            </FormItem>
-          )}
+    <>
+      <form className="grid grid-cols-1 gap-8 pt-2 sm:grid-cols-3 sm:space-y-8">
+        <ThemeOption
+          theme="system"
+          currentTheme={theme}
+          previewTheme={systemTheme}
+          text="System"
+          onClick={setTheme}
+        />
+        <ThemeOption
+          theme="light"
+          currentTheme={theme}
+          previewTheme="light"
+          text="Light"
+          onClick={setTheme}
+        />
+        <ThemeOption
+          theme="dark"
+          currentTheme={theme}
+          previewTheme="dark"
+          text="Dark"
+          onClick={setTheme}
         />
       </form>
-    </Form>
+    </>
   );
 }
--- src/app/(routes)/profile/security/page.tsx
+++ src/app/(routes)/profile/security/page.tsx
@@ -7,7 +7,7 @@ export default function SettingsSecurityPage() {
       <div>
         <h3 className="text-lg font-medium">Security</h3>
         <p className="text-sm text-muted-foreground">
-          Turn items on or off to control what&apos;s displayed in the app.
+          Turn items on or off to control what is displayed in the app.
         </p>
       </div>
       <Separator />
